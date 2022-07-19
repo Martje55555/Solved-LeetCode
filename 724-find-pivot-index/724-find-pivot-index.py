5f24fe8d17b9dca44a539c880d94e399d1ed9ec5
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-07-18 22:10:31 -0400

    Time: 126 ms (88.57%), Space: 14.4 MB (84.00%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        leftSum = 0
+        for i, x in enumerate(nums):
+            if leftSum == (total - leftSum)-x:
+                return i
+            leftSum += x
+        return -1
+                

f6a0528505fe65509b8340a3b8703efa38139c48
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-07-23 15:48:29 -0400

    Time: 30 ms (70.04%), Space: 15.5 MB (53.04%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -18,3 +18,44 @@ class Solution(object):
             prev = curr
             curr = next
         return prev   
+    
+    '''
+    curr = 1
+    prev = None
+    next = None
+    
+    1st iteration
+    next = 2
+    curr.next = None
+    prev = 1
+    curr = 2
+    
+    2nd iteration
+    next = 3
+    curr.next = 1
+    prev = 2
+    curr = 3
+    
+    3rd iteration
+    next = 4
+    curr.next = 2
+    prev = 3
+    curr = 4
+    
+    4th iteration
+    next = 5
+    curr.next = 3
+    prev = 4
+    curr = 5
+    
+    5th iteration
+    next = None
+    curr.next = 4
+    prev = 5
+    curr = None
+    
+    Done
+    
+    outcome: print(prev)
+    [5, 4, 3, 2, 1]
+    '''

9903a46409d12b747bda758e3371b18aa6a0f355
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-07-30 19:07:58 -0400

    Time: 453 ms (94.71%), Space: 13.5 MB (68.77%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        
         def findPalIndex(s, l, r):
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
             return l + 1, r - 1
         
-        start = 0
-        end = 0
+        start, end = 0, 0
         
         for i in range(len(s)):
             oddLeft, oddRight = findPalIndex(s, i, i)

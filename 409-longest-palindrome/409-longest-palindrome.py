52644133e75afe3cd3ce54b3b75b3afbf3da9e0c
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-07-31 22:13:03 -0400

    Time: 54 ms (6.77%), Space: 13.5 MB (63.81%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def longestPalindrome(self, s):
+        
         if len(s) == 1:
             return 1
-        newSet = set()
+        
+        hold = set()
         
         for i in range(len(s)):
-            if s[i] in newSet:
-                newSet.remove(s[i])
+            if s[i] in hold:
+                hold.remove(s[i])
             else:
-                newSet.add(s[i])
-        if len(newSet) <= 1 : return len(s)
+                hold.add(s[i])
+                
+        print(hold)
+                
+        if bool(len(hold) < 1):
+            return (len(s) - len(hold))
         
-        return len(s) - len(newSet) + 1
+        return len(s) - len(hold) + 1
